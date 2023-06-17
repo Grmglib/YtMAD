6f2044fea988e88a7a213b5cc311f3e80f9e20e9
GrmGlib <gabrielrm2000@hotmail.com>
2023-06-17 18:11:46 -0300

    Cors corrected

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.AspNetCore.Mvc;
+
 namespace YtMAD
 {
     public class Program
@@ -13,7 +15,12 @@ namespace YtMAD
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -22,6 +29,8 @@ namespace YtMAD
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            
+            app.UseCors("MyPolicy");
 
             app.UseHttpsRedirection();
 
